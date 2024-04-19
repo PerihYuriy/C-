bc5264481835d1f706cdeeeed0b4dc22644e3ed2
PerihYuriy <144880209+PerihYuriy@users.noreply.github.com>
2024-04-19 09:34:32 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,136 @@
-﻿//варіант 2
-using System;
+﻿using System;
 
-
-public enum DaysOfWeek
+public interface ILibraryItem
 {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+    void IssueItem();
+    void ReturnItem();
+    bool CheckStatus();
 }
 
-public class CalendarEvent
+public class Book : ILibraryItem
 {
-    public string EventName { get; set; }
-    public DaysOfWeek EventDay { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    private bool isIssued;
 
+    public event EventHandler<string> StatusChanged;
 
-    public CalendarEvent(string eventName, DaysOfWeek eventDay)
+    public Book(string title, string author, int year)
     {
-        EventName = eventName;
-        EventDay = eventDay;
+        Title = title;
+        Author = author;
+        Year = year;
+        isIssued = false;
     }
 
-    public void PrintEventDetails()
+    public void IssueItem()
     {
-        Console.WriteLine($"Event {EventName} will take place on {EventDay}.");
+        if (!isIssued)
+        {
+            isIssued = true;
+            StatusChanged?.Invoke(this, $"{Title} has been issued.");
+        }
+        else
+        {
+            throw new BookAlreadyIssuedException($"{Title} is already issued.");
+        }
+    }
+
+    public void ReturnItem()
+    {
+        if (isIssued)
+        {
+            isIssued = false;
+            StatusChanged?.Invoke(this, $"{Title} has been returned.");
+        }
+        else
+        {
+            throw new BookNotIssuedException($"{Title} is not issued.");
+        }
+    }
+
+    public bool CheckStatus()
+    {
+        return isIssued;
     }
 }
 
+public class BookAlreadyIssuedException : Exception
+{
+    public BookAlreadyIssuedException(string message) : base(message)
+    {
+    }
+}
+
+public class BookNotIssuedException : Exception
+{
+    public BookNotIssuedException(string message) : base(message)
+    {
+    }
+}
+
+public class LibraryUser
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+
+    public LibraryUser(string name, int id)
+    {
+        Name = name;
+        ID = id;
+    }
+}
+
+
+public class Student : LibraryUser
+{
+    public string Department { get; set; }
+
+    public Student(string name, int id, string department) : base(name, id)
+    {
+        Department = department;
+    }
+}
+
+public class Teacher : LibraryUser
+{
+    public string Department { get; set; }
+
+    public Teacher(string name, int id, string department) : base(name, id)
+    {
+        Department = department;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        CalendarEvent myEvent = new CalendarEvent("Birthday party", DaysOfWeek.Saturday);
+        Book book1 = new Book("DiscreteMathematics", "Scherbyna", 2023);
+        Book book2 = new Book("MathematicalAnalyzes", "Tarasiuk", 2024);
 
-        myEvent.PrintEventDetails();
+        book1.StatusChanged += (sender, message) => Console.WriteLine(message);
+        book2.StatusChanged += (sender, message) => Console.WriteLine(message);
+
+        try
+        {
+            book1.IssueItem();
+            book1.IssueItem(); 
+        }
+        catch (BookAlreadyIssuedException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            book1.ReturnItem();
+            book1.ReturnItem(); 
+        }
+        catch (BookNotIssuedException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
